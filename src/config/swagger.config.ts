881f7d8ef8c725568104920a6ff006daf87c780f
VictorNoe24 <noeflo60@gmail.com>
2026-05-16 22:44:47 -0600

    [ADD] implement prisma and swagger

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,34 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('Belux Backend API')
+    .setDescription('Belux Backend Documentation')
+    .setVersion('1.0.0')
+
+    // JWT
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+
+    customSiteTitle: 'Belux API Docs',
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+
+import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Prefix global API
+  app.setGlobalPrefix('api');
+
+  // Enable CORS
+  app.enableCors();
+
+  // Global validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // Swagger
+  setupSwagger(app);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
